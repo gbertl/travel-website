35ce4bde709f691f59eb490d253e4b81dd01e4dc
gilbertlc <gilbert.lacasandile@gmail.com>
2022-02-11 19:58:23 +0800

    day 4 PM build, final build

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,11 @@ const showMenu = () => {
 showMenu();
 
 const scrollHeader = () => {
-  window.addEventListener('scroll', () => {
+  window.scrollY >= 100
+    ? header.classList.add('scroll-header')
+    : header.classList.remove('scroll-header');
+
+  window.addEventListener('scroll', function () {
     const header = document.getElementById('header');
 
     this.scrollY >= 100
@@ -135,3 +139,30 @@ const themeChanger = () => {
 };
 
 themeChanger();
+
+const scrollReveal = () => {
+  const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2800,
+    reset: true,
+  });
+
+  sr.reveal(
+    '.home__data, .home__social-link, .home__info, .discover__container, .experience__data, .experience__overlay, .place__card, .sponsor__content, .footer__data, .footer__rights',
+    {
+      origin: 'top',
+      interval: 100,
+    }
+  );
+
+  sr.reveal('.about__data, .video__description, .subscribe__description', {
+    origin: 'left',
+  });
+
+  sr.reveal('.about__img-overlay, .video__content, .subscribe__form', {
+    origin: 'right',
+    interval: 100,
+  });
+};
+
+scrollReveal();
